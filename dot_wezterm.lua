ec80a30e166ed16bacec6d616595a5c074996dbc
Edwin Fuentes <efuentesamin@gmail.com>
2025-02-02 12:26:36 -0500

    :wrench: refactor: Wezterm adjustements

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,14 +7,14 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font("CodeNewRoman Nerd Font Mono")
-config.font_size = 12
+config.font_size = 16
 
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.95
-config.macos_window_background_blur = 10
+config.window_background_opacity = 0.79
+config.macos_window_background_blur = 15
 
 config.color_scheme = "Batman"
 
@@ -33,4 +33,3 @@ config.colors = {
 
 -- and finally, return the configuration to wezterm
 return config
-
