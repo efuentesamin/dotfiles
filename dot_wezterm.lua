82b1d125c8970691ecddee14a4c88d247ce85a2f
Edwin Fuentes <efuentesamin@gmail.com>
2025-02-03 19:04:17 -0500

    :recycle: refactor(nvim): Replaced feline.nvim with lualine.nvim

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -13,6 +13,13 @@ config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
 
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
 config.window_background_opacity = 0.79
 config.macos_window_background_blur = 15
 
