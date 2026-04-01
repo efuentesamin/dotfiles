352be290ced42a41c111ca42da2da8760b3087af
Edwin Fuentes <efuentesamin@gmail.com>
2026-04-01 18:30:23 -0500

    :bug: fix: Fixed some vim plugins

--- .config/nvim/lua/config/plugins/mini.lua
+++ .config/nvim/lua/config/plugins/mini.lua
@@ -2,7 +2,7 @@ return {
 	"echasnovski/mini.nvim",
 	version = "*",
 	config = function()
-		--require("mini.animate").setup()
+		require("mini.animate").setup()
 		require("mini.indentscope").setup()
 		require("mini.move").setup()
 	end,
