1938ce40b348494dd877524a1ddaddfb1b4e9246
Edwin Fuentes <efuentesamin@gmail.com>
2026-04-04 12:15:35 -0500

    :sparkles: feat: Tools install script

---
+++ init_container.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
+brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+brew install fzf
+brew install fd
+brew install bat
+brew install ripgrep
+brew install zoxide
+brew install git-delta
+brew install eza
+brew install thefuck
+brew install jesseduffield/lazygit/lazygit
+brew install jesseduffield/lazydocker/lazydocker
+brew install posting
+brew install nvim
+. $HOME/.cargo/env && cargo install rainfrog && cargo install --locked tree-sitter-cli
